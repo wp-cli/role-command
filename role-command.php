429d1f9ba21a27c1e9799af0351a1f1562cece0c
Thrijith Thankachan <thrijith13@gmail.com>
2019-04-20 18:02:58 +0530

    PHPCS: Update ruleset to exclude calsses from NamingConventions sniff Fix CS in role-command.php, src/Capabilities_Command.php, src/Role_Command.php

--- role-command.php
+++ role-command.php
@@ -4,9 +4,9 @@ if ( ! class_exists( 'WP_CLI' ) ) {
 	return;
 }
 
-$autoload = dirname( __FILE__ ) . '/vendor/autoload.php';
-if ( file_exists( $autoload ) ) {
-	require_once $autoload;
+$wpcli_role_autoloader = dirname( __FILE__ ) . '/vendor/autoload.php';
+if ( file_exists( $wpcli_role_autoloader ) ) {
+	require_once $wpcli_role_autoloader;
 }
 
 WP_CLI::add_command( 'cap', 'Capabilities_Command' );
