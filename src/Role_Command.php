80e23679bb3b78607c93f48b49fb71060c3a6576
Daniel Bachhuber <daniel.bachhuber@automattic.com>
2023-02-27 04:40:56 -0800

    Add explanation of what happens for `wp role reset` (#58)

--- src/Role_Command.php
+++ src/Role_Command.php
@@ -245,6 +245,12 @@ class Role_Command extends WP_CLI_Command {
 	/**
 	 * Resets any default role to default capabilities.
 	 *
+	 * Uses WordPress' `populate_roles()` function to put one or more
+	 * roles back into the state they were at in the a fresh
+	 * WordPress install. Removes any capabilities that were added,
+	 * and restores any capabilities that were removed. Custom roles
+	 * are not affected.
+	 *
 	 * ## OPTIONS
 	 *
 	 * [<role-key>...]
@@ -255,9 +261,17 @@ class Role_Command extends WP_CLI_Command {
 	 *
 	 * ## EXAMPLES
 	 *
-	 *     # Reset role.
+	 *     # Reset three roles.
 	 *     $ wp role reset administrator author contributor
-	 *     Success: Reset 1/3 roles.
+	 *     Restored 1 capability to and removed 0 capabilities from 'administrator' role.
+	 *     No changes necessary for 'author' role.
+	 *     No changes necessary for 'contributor' role.
+	 *     Success: 1 of 3 roles reset.
+	 *
+	 *     # Reset a custom role.
+	 *     $ wp role reset custom_role
+	 *     Custom role 'custom_role' not affected.
+	 *     Error: Must specify a default role to reset.
 	 *
 	 *     # Reset all default roles.
 	 *     $ wp role reset --all
